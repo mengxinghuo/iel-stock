36e5c1efe7aca70b50337750fa1a2c7fb326457b
zhangmingming <15026677294@163.com>
2018-06-21 11:00:04 +0700

    stock

---
+++ src/main/java/com/truck/dao/StockMapper.java
@@ -0,0 +1,17 @@
+package com.truck.dao;
+
+import com.truck.pojo.Stock;
+
+public interface StockMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Stock record);
+
+    int insertSelective(Stock record);
+
+    Stock selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Stock record);
+
+    int updateByPrimaryKey(Stock record);
+}
