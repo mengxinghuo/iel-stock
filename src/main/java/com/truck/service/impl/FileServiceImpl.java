88e23dca865c4971f87c8d140c2ff179f090f3b1
zhangmingming <15026677294@163.com>
2018-07-05 13:38:09 +0700

    模板2

--- src/main/java/com/truck/service/impl/FileServiceImpl.java
+++ src/main/java/com/truck/service/impl/FileServiceImpl.java
@@ -70,8 +70,9 @@ public class FileServiceImpl implements FileService {
         String fileExtensionName = fileName.substring(fileName.lastIndexOf(".") + 1);
 //        String uploadFileName = UUID.randomUUID().toString() + "." + fileExtensionName;
         Date date = new Date();
-        String dateStr = DateTimeUtil.dateToStr(date,"yyyy-MM-dd");
-        String uploadFileName = dateStr+fileName;
+//        String dateStr = DateTimeUtil.dateToStr(date,"yyyy-MM-dd");
+//        String uploadFileName = dateStr+fileName;
+        String uploadFileName = fileName;
         File fileDir = new File(path);
         if (!fileDir.exists()) {
             fileDir.setWritable(true);
@@ -111,9 +112,8 @@ public class FileServiceImpl implements FileService {
         String fileName = file.getOriginalFilename();
         String fileExtensionName = fileName.substring(fileName.lastIndexOf(".") + 1);
         Date date = new Date();
-//        String dateStr = DateTimeUtil.dateToStr(date,"yyyy-MM-dd");
-//        String uploadFileName = dateStr+fileName;
-        String uploadFileName = fileName;
+        String dateStr = DateTimeUtil.dateToStr(date,"yyyy-MM-dd");
+        String uploadFileName = dateStr+fileName;
         File fileDir = new File(path);
         if (!fileDir.exists()) {
             fileDir.setWritable(true);
