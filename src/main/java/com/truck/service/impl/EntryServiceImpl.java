58308ce330918e5c8d4996cfc7528e2e5dae2cf8
zhangmingming <15026677294@163.com>
2018-09-17 13:48:19 +0800

    null

--- src/main/java/com/truck/service/impl/EntryServiceImpl.java
+++ src/main/java/com/truck/service/impl/EntryServiceImpl.java
@@ -54,6 +54,7 @@ public class EntryServiceImpl implements IEntryService {
         List<EntryVo> entryVoList = Lists.newArrayList();
         for(Entry entryItem : entryList){
             EntryVo entryVo = this.assembleEntry(entryItem);
+            if(!StringUtils.isEmpty(entryVo.getEntryNo()))
             entryVoList.add(entryVo);
         }
         PageInfo pageInfo = new PageInfo(entryList);
@@ -189,6 +190,8 @@ public class EntryServiceImpl implements IEntryService {
 
     public EntryVo assembleEntry(Entry entry){
         EntryVo entryVo = new EntryVo();
+        if(entry.getEntryNo() ==null)
+            return entryVo;
         entryVo.setId(entry.getId());
         entryVo.setEntryNo(entry.getEntryNo());
         entryVo.setDeclareNum(entry.getDeclareNum());
